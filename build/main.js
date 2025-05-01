c163a90abc4d4b1c29da27a1101e63aa202bfc1d
Crazyafk <fel3web@gmail.com>
2025-05-01 19:03:29 +0100

    add spell attributes

--- build/main.js
+++ build/main.js
@@ -1,11 +1,61 @@
 "use strict";
-var SpellSchools;
-(function (SpellSchools) {
-})(SpellSchools || (SpellSchools = {}));
+var SpellSchool;
+(function (SpellSchool) {
+    SpellSchool[SpellSchool["Abjuration"] = 0] = "Abjuration";
+    SpellSchool[SpellSchool["Arcanomancy"] = 1] = "Arcanomancy";
+    SpellSchool[SpellSchool["Astromancy"] = 2] = "Astromancy";
+    SpellSchool[SpellSchool["Conjuration"] = 3] = "Conjuration";
+    SpellSchool[SpellSchool["Chronurgy"] = 4] = "Chronurgy";
+    SpellSchool[SpellSchool["Divination"] = 5] = "Divination";
+    SpellSchool[SpellSchool["Enchantment"] = 6] = "Enchantment";
+    SpellSchool[SpellSchool["Evocation"] = 7] = "Evocation";
+    SpellSchool[SpellSchool["Haemocraft"] = 8] = "Haemocraft";
+    SpellSchool[SpellSchool["Illusion"] = 9] = "Illusion";
+    SpellSchool[SpellSchool["Mythcraft"] = 10] = "Mythcraft";
+    SpellSchool[SpellSchool["Necromancy"] = 11] = "Necromancy";
+    SpellSchool[SpellSchool["Technomancy"] = 12] = "Technomancy";
+    SpellSchool[SpellSchool["Transmutation"] = 13] = "Transmutation";
+})(SpellSchool || (SpellSchool = {}));
+var SpellList;
+(function (SpellList) {
+    SpellList[SpellList["Artificer"] = 0] = "Artificer";
+    SpellList[SpellList["Bloodbound"] = 1] = "Bloodbound";
+    SpellList[SpellList["Cleric"] = 2] = "Cleric";
+    SpellList[SpellList["Paladin"] = 3] = "Paladin";
+    SpellList[SpellList["Psycaster"] = 4] = "Psycaster";
+    SpellList[SpellList["SpellSword"] = 5] = "SpellSword";
+    SpellList[SpellList["Wizard"] = 6] = "Wizard";
+})(SpellList || (SpellList = {}));
+var SpellComponent;
+(function (SpellComponent) {
+    SpellComponent[SpellComponent["F"] = 0] = "F";
+    SpellComponent[SpellComponent["Feq"] = 1] = "Feq";
+    SpellComponent[SpellComponent["V"] = 2] = "V";
+    SpellComponent[SpellComponent["S"] = 3] = "S";
+    SpellComponent[SpellComponent["S2"] = 4] = "S2";
+    SpellComponent[SpellComponent["M"] = 5] = "M";
+    SpellComponent[SpellComponent["Mc"] = 6] = "Mc";
+})(SpellComponent || (SpellComponent = {}));
+var Concentration;
+(function (Concentration) {
+    Concentration[Concentration["None"] = 0] = "None";
+    Concentration[Concentration["Half"] = 1] = "Half";
+    Concentration[Concentration["Full"] = 2] = "Full";
+})(Concentration || (Concentration = {}));
 class Spell {
-    constructor(level, name) {
+    constructor(level, name, school, description, lists, castingtime, components, concentration, range, duration) {
+        this.lists = [];
+        this.components = [];
         this.level = level;
         this.name = name;
+        this.school = school;
+        this.description = description;
+        this.lists = lists;
+        this.castingtime = castingtime;
+        this.components = components;
+        this.concentration = concentration;
+        this.range = range;
+        this.duration = duration;
     }
 }
 function buttonclick() {
--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,44 @@
-enum SpellSchools{
-        
+enum SpellSchool{
+    Abjuration, Arcanomancy, Astromancy, Conjuration, Chronurgy, Divination, 
+    Enchantment, Evocation, Haemocraft, Illusion, Mythcraft,
+    Necromancy, Technomancy, Transmutation
+}
+enum SpellList{
+    Artificer, Bloodbound, Cleric, Paladin, Psycaster, SpellSword, Wizard
+}
+enum SpellComponent{
+    F, Feq, V, S, S2, M, Mc
+}
+enum Concentration{
+    None, Half, Full
 }
 class Spell
 {
     level: number
     name: string
+    school: SpellSchool
+    description: string
+    lists: SpellList[] = []
+    castingtime: string
+    components: SpellComponent[] = []
+    concentration: Concentration
+    range: string
+    duration: string
 
-    constructor(level: number, name: string)
+    constructor(level: number, name: string, school: SpellSchool, description: string, 
+        lists: SpellList[], castingtime: string, components: SpellComponent[],
+        concentration: Concentration, range: string, duration: string)
     {
         this.level = level
         this.name = name
+        this.school = school
+        this.description = description
+        this.lists = lists
+        this.castingtime = castingtime
+        this.components = components
+        this.concentration = concentration
+        this.range = range
+        this.duration = duration
     }
 }
 function buttonclick()
